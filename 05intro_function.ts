f0250fa7563afb55a1c064fc262f3cf5bcc4e335
Prabhakar1437 <prabhakarvatsking@gmail.com>
2024-04-25 13:08:45 +0530

    DAY1

---
+++ 05intro_function.ts
@@ -0,0 +1,38 @@
+//! Function Invocation:
+//* To execute a function, you simply invoke it by using its name followed by  parenthesis.
+
+//* you can provide arguments(actual value) for the parameters defined in the function declaration.
+
+//!Function Declaration:
+//*you can declare a function using function keyword followed by function name, a list of parameters enclosed in parenthesis, and a return type.
+//* The Function body contains the code that will be executed when the function is called.
+
+//declaration ke time jo define karte h oo perameters hai
+// function greet(name:string, id:number ){
+//     console.log(`Hello ${name} your id is ${id}`);
+
+
+// }
+// function call ke time jo define karte h oo arguments.
+
+// const greet = (name:string, id:number ) =>{
+//     console.log(`Hello ${name} your id is ${id}`);
+// }
+// greet("Vats",1)
+
+
+//*Function return type : To specify the type of function , you can use the colon(:) followed by the desired type after the function's parameter list.
+
+const greet = (name:string, id:number ):string =>{
+     return `Hello ${name} your id is ${id}`;
+}
+console.log(greet("Vats",1))
+
+// Palindrome
+
+const  isPalindrome = (palin:string):boolean=> {
+    let myPalin = palin.split("").reverse().join("");
+    return myPalin ===palin
+}
+console.log(isPalindrome("racecar")); 
+ 
---
+++ 06typeinference.ts
@@ -0,0 +1,7 @@
+//*Type inference 
+//*Type inference in typescript refers to the ability of the TypeScript compiler to automatically determine and assign types to the variables, expressions, and function return values based on their usage and context in the code.
+
+
+let myNameis ='Vinod'
+
+let myFavNum1 = 9999
